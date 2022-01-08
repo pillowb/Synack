bd65b5176c8a1123797f849dda2b6cf5bdb5a87e
pillowb <97346439+pillowb@users.noreply.github.com>
2022-01-08 21:57:06 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from Forms import CreateUserForm
+import shelve, User
+
+app = Flask(__name__)
+app.secret_key = 'any_random_string'
+
+@app.route('/createUser', methods=['GET', 'POST'])
+def create_user():
+    create_user_form = CreateUserForm(request.form)
+    if request.method == 'POST' and create_user_form.validate():
+        users_dict = {}
+        db = shelve.open('storage.db', 'c')
+
+        try:
+            users_dict = db['Users']
+        except:
+            print("Error in retrieving Users from storage.db.")
+
+        if len(users_dict) > 0:
+            User.User.count_id = list(users_dict.keys())[-1]
+
+        user = User.User(create_user_form.first_name.data, create_user_form.last_name.data, create_user_form.gender.data, create_user_form.membership.data, create_user_form.remarks.data)
+        users_dict[user.get_user_id()] = user
+        db['Users'] = users_dict
+
+        db.close()
+
+        session['user_created'] = user.get_first_name() + ' ' + user.get_last_name()
+
+        return redirect(url_for('home'))
+    return render_template('createUser.html', form=create_user_form)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contactUs')
+def contact_us():
+    return render_template('contactUs.html')
+
+
+@app.route('/retrieveUsers')
+def retrieve_users():
+    users_dict = {}
+    db = shelve.open('storage.db', 'r')
+    users_dict = db['Users']
+    db.close()
+
+    users_list = []
+    for key in users_dict:
+        user = users_dict.get(key)
+        users_list.append(user)
+
+    return render_template('retrieveUsers.html', count=len(users_list), users_list=users_list)
+
+
+@app.route('/updateUser/<int:id>/', methods=['GET', 'POST'])
+def update_user(id):
+    update_user_form = CreateUserForm(request.form)
+    if request.method == 'POST' and update_user_form.validate():
+        users_dict = {}
+        db = shelve.open('storage.db', 'w')
+        users_dict = db['Users']
+
+        user = users_dict.get(id)
+        user.set_first_name(update_user_form.first_name.data)
+        user.set_last_name(update_user_form.last_name.data)
+        user.set_gender(update_user_form.gender.data)
+        user.set_membership(update_user_form.membership.data)
+        user.set_remarks(update_user_form.remarks.data)
+
+        db['Users'] = users_dict
+        db.close()
+        session['user_updated'] = user.get_first_name() + ' ' + user.get_last_name()
+        return redirect(url_for('retrieve_users'))
+
+    else:
+        users_dict = {}
+        db = shelve.open('storage.db', 'r')
+        users_dict = db['Users']
+        db.close()
+
+        user = users_dict.get(id)
+        update_user_form.first_name.data = user.get_first_name()
+        update_user_form.last_name.data = user.get_last_name()
+        update_user_form.gender.data = user.get_gender()
+        update_user_form.membership.data = user.get_membership()
+        update_user_form.remarks.data = user.get_remarks()
+
+        return render_template('updateUser.html', form=update_user_form)
+
+
+@app.route('/deleteUser/<int:id>', methods=['POST'])
+def delete_user(id):
+    users_dict = {}
+    db = shelve.open('storage.db', 'w')
+    users_dict = db['Users']
+
+    user = users_dict.pop(id)
+
+    db['Users'] = users_dict
+    db.close()
+    session['user_deleted'] = user.get_first_name() + ' ' + user.get_last_name()
+    return redirect(url_for('retrieve_users'))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run()
